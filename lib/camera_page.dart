ef41df7393337901c94da022739dcdc1a29be1ff
shivhub09 <shivnagori2020@gmail.com>
2023-12-01 19:50:17 +0530

    recording video and camera done

--- lib/camera_page.dart
+++ lib/camera_page.dart
@@ -11,6 +11,8 @@ class CameraPage extends StatefulWidget {
 class _CameraPageState extends State<CameraPage> {
   late List<CameraDescription> cameras;
   late CameraController? _controller; // Make the controller nullable
+  int _currentCameraIndex = 0;
+  late bool _isRecording = false;
 
   @override
   void initState() {
@@ -20,7 +22,8 @@ class _CameraPageState extends State<CameraPage> {
 
   Future<void> initializeCamera() async {
     cameras = await availableCameras();
-    _controller = CameraController(cameras[0], ResolutionPreset.medium);
+    _controller =
+        CameraController(cameras[_currentCameraIndex], ResolutionPreset.medium);
     await _controller!.initialize();
     if (mounted) {
       setState(() {});
@@ -33,6 +36,52 @@ class _CameraPageState extends State<CameraPage> {
     super.dispose();
   }
 
+  Future<void> _toggleCamera() async {
+    _currentCameraIndex = (_currentCameraIndex + 1) % cameras.length;
+    await _controller!.dispose();
+    _controller =
+        CameraController(cameras[_currentCameraIndex], ResolutionPreset.medium);
+    await _controller!.initialize();
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+// video :
+  Future<void> _startRecording() async {
+    if (!_controller!.value.isInitialized ||
+        _controller!.value.isRecordingVideo) {
+      return;
+    }
+
+    try {
+      await _controller!.startVideoRecording();
+      setState(() {
+        _isRecording = true;
+      });
+    } on CameraException catch (e) {
+      print('Error starting video recording: $e');
+      setState(() {
+        _isRecording = false;
+      });
+    }
+  }
+
+  Future<void> _stopRecording() async {
+    if (!_controller!.value.isRecordingVideo) {
+      return null;
+    }
+
+    try {
+      await _controller?.stopVideoRecording();
+      setState(() {
+        _isRecording = false;
+      });
+    } on CameraException catch (e) {
+      print('Error stopping video recording: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     if (_controller == null || !_controller!.value.isInitialized) {
@@ -41,15 +90,33 @@ class _CameraPageState extends State<CameraPage> {
       );
     }
     return Scaffold(
-      body: SizedBox.expand(
-        child: FittedBox(
-          fit: BoxFit.cover,
-          child: SizedBox(
-            width: _controller!.value.previewSize!.height,
-            height: _controller!.value.previewSize!.width,
-            child: CameraPreview(_controller!),
+      backgroundColor: Colors.black,
+      floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.white,
+          onPressed: _toggleCamera,
+          child: Icon(
+            Icons.switch_camera,
+            color: Colors.black,
+          )),
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          Center(child: CameraPreview(_controller!)),
+          Positioned(
+            bottom: 10.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.circle),
+                  iconSize: 60.0,
+                  color: _isRecording ? Colors.red : Colors.white,
+                  onPressed: _isRecording ? _stopRecording : _startRecording,
+                ),
+              ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
