3633f44b3885c7086ec835f3a267c11565680a1c
shivhub09 <shivnagori2020@gmail.com>
2023-12-02 11:18:44 +0530

    edits to the code

--- lib/camera_page.dart
+++ lib/camera_page.dart
@@ -1,5 +1,12 @@
+import 'dart:async';
+import 'dart:io';
+import 'dart:typed_data';
+import 'dart:ui';
+import 'package:image/image.dart' as IMG;
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:native_screenshot/native_screenshot.dart';
+import 'package:tflite/tflite.dart';
 
 class CameraPage extends StatefulWidget {
   const CameraPage({Key? key}) : super(key: key);
@@ -11,8 +18,16 @@ class CameraPage extends StatefulWidget {
 class _CameraPageState extends State<CameraPage> {
   late List<CameraDescription> cameras;
   late CameraController? _controller; // Make the controller nullable
-  int _currentCameraIndex = 0;
-  late bool _isRecording = false;
+  late int _currentCameraIndex = 0;
+
+  late bool _recording = false;
+  late Timer timer;
+  String output = "";
+  String prevOutput = "";
+  String translation = "";
+  double confidenceScore = 0.0;
+  Color boxColor = Colors.black;
+  bool steadyTextDisplay = false;
 
   @override
   void initState() {
@@ -57,12 +72,12 @@ class _CameraPageState extends State<CameraPage> {
     try {
       await _controller!.startVideoRecording();
       setState(() {
-        _isRecording = true;
+        _recording = true;
       });
     } on CameraException catch (e) {
       print('Error starting video recording: $e');
       setState(() {
-        _isRecording = false;
+        _recording = false;
       });
     }
   }
@@ -75,49 +90,177 @@ class _CameraPageState extends State<CameraPage> {
     try {
       await _controller?.stopVideoRecording();
       setState(() {
-        _isRecording = false;
+        _recording = false;
       });
     } on CameraException catch (e) {
       print('Error stopping video recording: $e');
     }
   }
+// video ended
+
+//////////////////////////////////////////
+  _recordVideo() async {
+    if (_recording) {
+      timer.cancel();
+
+      setState(() => _recording = false);
+    } else {
+      setState(() => _recording = true);
+      translation = "";
+      timer = Timer.periodic(const Duration(seconds: 5), (timer) async {
+        String? path = await NativeScreenshot.takeScreenshot();
+
+        if (path == null || path.isEmpty) {
+          print("Screenshot didnt work");
+        }
+
+        File imgFile = File(path!);
+        // Cropping the image
+        Uint8List bytes = imgFile.readAsBytesSync();
+        IMG.Image? src = IMG.decodeImage(bytes);
+
+        if (src != null) {
+          // IMG.Image destImage = IMG.copyCrop(src, 300, 990, 560, 560);
+          IMG.Image destImage =
+              IMG.copyCrop(src, x: 300, y: 990, width: 560, height: 560);
+          var jpg = IMG.encodeJpg(destImage);
+          // var res  = await imageToByteListFloat32(destImage, 560, 0.0, 255.0);
+
+          // path = "../assets/images/IMG_4188.jpg";
+          // Uint8List myGesture = File(path).readAsBytesSync();
+          // IMG.Image? myImage = IMG.decodeImage(myGesture);
+          // IMG.Image resizedImage = IMG.copyResize(myImage!, width:64, height:64);
+          IMG.Image resizedImage =
+              IMG.copyResize(destImage, width: 64, height: 64);
+          var res = await Tflite.runModelOnBinary(
+              binary: imageToByteListFloat32(resizedImage, 64, 0.0, 255.0),
+              numResults: 29);
+          if (res != null) {
+            output = res[0]['label'];
+            steadyTextDisplay = true;
+            confidenceScore = res[0]['confidence'];
+            if (confidenceScore > 0.85) {
+              // change box colourto green
+              boxColor = Colors.green;
+              steadyTextDisplay = false;
+              if (output != prevOutput && output.length == 1) {
+                prevOutput = output;
+                translation = translation + output;
+              }
+            } else if (confidenceScore > 0.6) {
+              // change box colour to yellow
+              boxColor = Colors.yellow;
+            } else {
+              // change box colour to red
+              boxColor = Colors.red;
+            }
+            setState(() {});
+          }
+          // File croppedImage = await File(imgFile.path).writeAsBytes(jpg);
+        }
+      });
+    }
+  }
+
+//////////////////////////////////////////
+
+  Uint8List imageToByteListFloat32(
+      IMG.Image img, int inputSize, double mean, double std) {
+    var convertedBytes = Float32List(1 * inputSize * inputSize * 3);
+    var buffer = Float32List.view(convertedBytes.buffer);
+    int pixelIndex = 0;
+    for (var i = 0; i < inputSize; i++) {
+      for (var j = 0; j < inputSize; j++) {
+        final data = img.getBytes();
+        var pixel = img.getPixel(j, i);
+        buffer[pixelIndex++] = (data[pixelIndex] - mean) / std;
+        buffer[pixelIndex++] = (data[pixelIndex] - mean) / std;
+        buffer[pixelIndex++] = (data[pixelIndex] - mean) / std;
+      }
+    }
+    return convertedBytes.buffer.asUint8List();
+  }
+
+  Future<void> switchCamera() async {
+    // Dispose the current controller before switching
+    await _controller?.dispose();
+    _currentCameraIndex = (_currentCameraIndex + 1) % cameras.length;
+    _controller =
+        CameraController(cameras[_currentCameraIndex], ResolutionPreset.medium);
+    await _controller!.initialize();
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+//////////////////////////////////////////
+
+  // (Your imports remain unchanged)
 
   @override
   Widget build(BuildContext context) {
-    if (_controller == null || !_controller!.value.isInitialized) {
+    if (!_controller!.value.isInitialized) {
       return const Center(
         child: CircularProgressIndicator(),
       );
-    }
-    return Scaffold(
-      backgroundColor: Colors.black,
-      floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.white,
-          onPressed: _toggleCamera,
-          child: Icon(
-            Icons.switch_camera,
-            color: Colors.black,
-          )),
-      body: Stack(
-        alignment: Alignment.center,
-        children: [
-          Center(child: CameraPreview(_controller!)),
-          Positioned(
-            bottom: 10.0,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                IconButton(
-                  icon: Icon(Icons.circle),
-                  iconSize: 60.0,
-                  color: _isRecording ? Colors.red : Colors.white,
-                  onPressed: _isRecording ? _stopRecording : _startRecording,
+    } else {
+      return Scaffold(
+        backgroundColor: Colors.black12,
+        floatingActionButton: FloatingActionButton(onPressed: _toggleCamera),
+        body: Stack(
+          children: [
+            Center(child: CameraPreview(_controller!)),
+            Positioned(
+              left: 16,
+              top: 16,
+              child: AnimatedOpacity(
+                duration: const Duration(milliseconds: 500),
+                opacity: steadyTextDisplay ? 1.0 : 0.0,
+                child: Text(
+                  'Keep steady for accurate results',
+                  style: TextStyle(fontSize: 16),
                 ),
-              ],
+              ),
             ),
-          ),
-        ],
-      ),
-    );
+            Align(
+              alignment: Alignment.center,
+              child: Container(
+                width: 200,
+                height: 200,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: boxColor,
+                    width: 5,
+                  ),
+                ),
+              ),
+            ),
+            Align(
+              alignment: const Alignment(0, 0.7),
+              child: AnimatedSwitcher(
+                duration: const Duration(milliseconds: 500),
+                child: Text(
+                  translation,
+                  key: Key(
+                      translation), // Ensure proper animation on text change
+                  style: const TextStyle(fontSize: 25),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 16),
+                child: ElevatedButton(
+                  onPressed: _recordVideo,
+                  child:
+                      Text(_recording ? 'Stop Recording' : 'Start Recording'),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,109 @@
+import 'dart:typed_data';
+import 'dart:ui' as ui; // Renamed 'dart:ui' alias
 import 'package:flutter/material.dart';
-import 'package:gesture/camera_page.dart';
+import 'package:flutter/services.dart';
+import 'package:image/image.dart' as IMG;
+import 'package:tflite/tflite.dart';
+import 'camera_page.dart'; // Assuming this is your own file
+
+void loadDetectionModel() async {
+  Tflite.close();
+  try {
+    await Tflite.loadModel(
+      model: "assets/model.tflite",
+      labels: "assets/labels.txt",
+    );
+    print("Loaded model successfully");
+    testModel(); // Corrected function name to lowercase
+  } on Exception catch (e) {
+    print("Failed to load model: $e");
+  }
+}
+
+Uint8List imageToByteListFloat32(
+  IMG.Image img,
+  int inputSize,
+  double mean,
+  double std,
+) {
+  var convertedBytes = Float32List(1 * inputSize * inputSize * 3);
+  var buffer = Float32List.view(convertedBytes.buffer);
+  int count = 0;
+  int pixelIndex = 0;
+  for (var i = 0; i < inputSize; i++) {
+    print("running i loop");
+    for (var j = 0; j < inputSize; j++) {
+      print("running i loop");
+
+      final data = img.getBytes();
+      // Ensure the index is within the bounds of the data list
+      if (pixelIndex < data.length) {
+        var pixel = img.getPixel(j, i);
+        count = count + 1;
+        buffer[pixelIndex++] = (data[pixelIndex] - mean) / std;
+        buffer[pixelIndex++] = (data[pixelIndex] - mean) / std;
+        buffer[pixelIndex++] = (data[pixelIndex] - mean) / std;
+      } else {
+        print('Index out of bounds: $pixelIndex');
+        print(count);
+
+        // Handle or debug the out-of-bounds issue here
+      }
+    }
+  }
+  print(count);
+  return convertedBytes.buffer.asUint8List();
+}
+
+Future<IMG.Image> addAndResize(String s) async {
+  var img = IMG.decodeImage((await rootBundle.load(s)).buffer.asUint8List());
+  return IMG.copyResize(img!, height: 64, width: 64);
+}
+
+int getRed(int color) {
+  return (color >> 16) & 0xFF;
+}
+
+int getGreen(int color) {
+  return (color >> 8) & 0xFF;
+}
+
+int getBlue(int color) {
+  return color & 0xFF;
+}
+
+void testModel() async {
+  print("testing model");
+  IMG.Image resizeImage;
+  try {
+    String ALPHA = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    int score = 0;
+    for (int i = 0; i < 26; i++) {
+      String imgSrc = "assets/images/" + ALPHA[i] + "_test.jpg";
+      resizeImage = await addAndResize(imgSrc);
+      var res = await Tflite.runModelOnBinary(
+        binary: imageToByteListFloat32(resizeImage, 64, 0.0, 255.0),
+        numResults: 29,
+      );
+      if (res?[0]['label'] == ALPHA[i]) {
+        score++;
+      }
+    }
+    print("Model Score: ${(score / 26) * 100}%");
+  } catch (e) {
+    print("runModelError: $e");
+  }
+}
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-
   runApp(const MyApp());
+  loadDetectionModel();
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key); // Added Key parameter
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
